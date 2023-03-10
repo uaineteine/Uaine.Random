3ae1b9c9e7c8ef8a799f494f538584d1d76f5943
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2023-03-10 19:13:17 +1100

    --1.1.2-- Uaine.Objects dependency updated to 1.11 from 1.7 min Net Standard now 1.3 from 1 added a general MakeBuffer method that uses the base NextBytes for a set buffer length

--- Uaine.Random/URandom.cs
+++ Uaine.Random/URandom.cs
@@ -5,7 +5,7 @@ namespace Uaine.Random
 {
     public class URandom : SeededRandom
     {
-        protected bool usingrandomseed;
+        protected bool usingrandomseed = false;
         public bool UsingRandomSeed => usingrandomseed;
         public URandom() : base(randomseed())
         {
@@ -13,11 +13,9 @@ namespace Uaine.Random
         }
         public URandom(string seed) : base(seed)
         {
-            usingrandomseed = false;
         }
         public URandom(int seed) : base(seed)
         {
-            usingrandomseed = false;
         }
 
         private static int randomseed()
@@ -25,36 +23,33 @@ namespace Uaine.Random
             System.Random rnd = new System.Random();
             return rnd.Next();
         }
+
+        public byte[] MakeBuffer(int length)
+        {
+            byte[] buffer = new byte[length];
+            NextBytes(buffer);
+            return buffer;
+        }
         public long RandomLong()
         {
-            byte[] buffer = new byte[8];
-            NextBytes(buffer);
-            return BitConverter.ToInt64(buffer, 0);
+            return BitConverter.ToInt64(MakeBuffer(8), 0);
         }
         public ulong RandomULong()
-        {
-            byte[] buffer = new byte[8];
-            NextBytes(buffer);
-            return BitConverter.ToUInt64(buffer, 0);
+        { 
+            return BitConverter.ToUInt64(MakeBuffer(8), 0);
         }
         public long RandomLongPosOnly()
         {
-            byte[] buffer = new byte[8];
-            NextBytes(buffer);
-            long value = BitConverter.ToInt64(buffer, 0);
+            long value = BitConverter.ToInt64(MakeBuffer(8), 0);
             return Math.Abs(value);
         }
         public short RandomShort()
         {
-            byte[] buffer = new byte[2];
-            NextBytes(buffer);
-            return BitConverter.ToInt16(buffer, 0);
+            return BitConverter.ToInt16(MakeBuffer(2), 0);
         }
         public ushort RandomUShort()
         {
-            byte[] buffer = new byte[2];
-            NextBytes(buffer);
-            return BitConverter.ToUInt16(buffer, 0);
+            return BitConverter.ToUInt16(MakeBuffer(2), 0);
         }
         public bool RandomBool()
         {
