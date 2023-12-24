7b89902933dad1d42938d8290399d74f2a6ea996
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2023-12-24 14:38:19 +1100

    --2.0 dev-- Standalone package, no longer requires any dependencies seeded random class from uaine.objects deployed into package new die class lowered .net standard to 1.0 for more compatability

---
+++ Uaine.Random/Die.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Uaine.Random
+{
+    public class Die
+    {
+        private readonly URandom random;
+        private readonly int sides;
+        private int modifier = 0;
+
+        public int Modifier
+        {
+            get { return modifier; }
+            set { modifier = value; }
+        }
+
+        public Die(int sides, int modifier = 0)
+        {
+            if (sides <= 0)
+            {
+                throw new ArgumentException("Number of sides must be greater than zero.");
+            }
+
+            this.sides = sides;
+            random = new URandom();
+            this.modifier = modifier;
+        }
+
+        public Die(int sides, string seed, int modifier = 0)
+        {
+            if (sides <= 0)
+            {
+                throw new ArgumentException("Number of sides must be greater than zero.");
+            }
+
+            this.sides = sides;
+            random = new URandom(seed);
+            this.modifier = modifier;
+        }
+
+        public Die(int sides, int seed, int modifier = 0)
+        {
+            if (sides <= 0)
+            {
+                throw new ArgumentException("Number of sides must be greater than zero.");
+            }
+
+            this.sides = sides;
+            random = new URandom(seed);
+            this.modifier = modifier;
+        }
+
+        public int Roll()
+        {
+            return random.Next(1, sides + 1) + modifier;
+        }
+
+        public int Max(int numberOfRolls)
+        {
+            int maxResult = 0;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int rollResult = Roll();
+                if (rollResult > maxResult)
+                {
+                    maxResult = rollResult;
+                }
+            }
+            return maxResult;
+        }
+
+        public int Sum(int numberOfRolls)
+        {
+            int sum = 0;
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                sum += Roll();
+            }
+            return sum;
+        }
+    }
+}
